b59a61a2747c4ce661cebcf171367ad9bb5bf5a2
Ana Lidia Lisboa de Andrade <analidialisboadeand@gmail.com>
2023-06-08 16:36:41 -0300

    Adicionando Cube3D

---
+++ src/home/index.js
@@ -0,0 +1,28 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { Button } from 'reactstrap';
+import { useNavigate } from 'react-router-dom';
+import { ButtonStyle } from  './style';
+
+export const Menu = () => {
+    const navigate = useNavigate();
+
+    const toCube = () => {
+        navigate("/cube")
+    }
+
+    const toEarth = () => {
+        navigate("/earth")
+    }
+
+    return (
+        <>
+            <ButtonStyle>
+                <Button color="danger" onCLick={toCube}>Cubo Giratório</Button>
+                <Button color="danger" onCLick={toEarth}>3D Model</Button>
+            </ButtonStyle>
+        </>
+    )
+}
+
+export default Menu;
---
+++ src/pages/cube/cube.js
@@ -0,0 +1,35 @@
+import React, {useRef, useState} from "react";
+import { useFrame } from "@react-three/fiber";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+
+function Cube3d (props) {
+    const ref = useRef();
+
+    const [hovered, setHovered] = useState(false);
+    const [clicked, setClicked] = useState(false);
+
+    useFrame((state,delta) => (ref.current.rotation.x += 0.01));
+
+    return (
+        <>
+        <mesh
+            {...props}
+            ref={ref}
+            scale={clicked ? 1.5 : 1}
+            onClick={(event) => setClicked(!clicked)}
+            onPointerOver={(event) => setHovered(true)}
+            onPointerOut={(event) => setHovered(false)}
+        >
+            <OrbitControls target = {[0, 0.35, 0]} maxPolarAngle={1.45}/>
+            <PerspectiveCamera makeDefault fav= {50} position={[3, 2, 5]}/>
+            <boxGeometry args={[1, 1, 1]}/>
+            <meshStandardMaterial
+                wireframe={props.wireframe}
+                color={hovered ? "blue" : "orange"}
+            />
+        </mesh>
+        </>
+    );
+}
+
+export default Cube3d;
