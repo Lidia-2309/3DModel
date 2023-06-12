9a84473dd366969b879271b61b68fca5c69cb6f7
Lidia-2309 <analidialisboadeand@gmail.com>
2023-06-12 11:39:07 -0300

    Aplicando zoom ao model 3d

--- src/pages/cube/index.js
+++ src/pages/cube/index.js
@@ -1,6 +1,7 @@
 import { Canvas } from "@react-three/fiber";
 import Cube3d from "./cube";
 import music from "../../assets/music.wav";
+import { OrbitControls } from '@react-three/drei'
 
 function play (){
     new Audio(music).play()
@@ -14,8 +15,13 @@ function CubeApp () {
             </div>
             <section>
                 <Canvas>
-                    <pointLight position={[20,20,10]}/>
-                    <Cube3d position={[-1.2, 0, 2]}/>
+                    {/* <pointLight position={[20,20,10]}/> */}
+                    <ambientLight intensity={0.5} />
+                    <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+                    <pointLight position={[-10, -10, -10]} />
+                    <Cube3d position={[-1.2, 0, 0]}/>
+                    <OrbitControls />
+                    
                 </Canvas>
             </section>
         </>
--- src/pages/earth/earth.js
+++ src/pages/earth/earth.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { useGLTF, Stage, PresentationControls } from "@react-three/drei";
+import { OrbitControls } from '@react-three/drei'
 
 function Model (props) {
     const {scene} = useGLTF("skyHome.glb");
@@ -16,10 +17,13 @@ const EarthPage = () => {
                     shadows camera={{fav:1000}}
                     style={{"position":"absolute"}}
                 >
+                    <OrbitControls />
                     <PresentationControls
-                        speed={3}
+                        speed={1}
                         global zoom={0.5}
                         polar={[-100, Math.PI / 4]}
+                        cursor={true} 
+                        
                     >
                         <Stage environment={null}>
                             <Model scale={0.05} />
